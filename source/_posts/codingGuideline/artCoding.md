098c934739e1aff15d273aa710633fc6e1167791
fulndon <1021131482@qq.com>
2018-05-20 01:15:17 +0800

    html

--- source/_posts/codingGuideline/artCoding.md
+++ source/_posts/codingGuideline/artCoding.md
@@ -7,6 +7,6 @@ date: 2018-05-12 08:45:24
 tags:
 ---
 ```
-[开发规范手册](/pdfjs/web/viewer.html "title")
+[开发规范手册](/pdfjs/self/web/viewer.html "title")
 ```
-<a target="_blank" href="/pdfjs/web/viewer.html">开发规范手册</a>
+<a target="_blank" href="/self/pdfjs/web/viewer.html">开发规范手册</a>
---
+++ source/self/js/jquery/pwd.js
@@ -0,0 +1,33 @@
+﻿var checkPwd = function (path,input) {
+		var result = false;
+        $.ajax({
+            url: path,
+            dataType: "xml",
+            async: false,
+            success: function (xmlResponse) {
+                var datas = $( "entry", xmlResponse ).map(function() {
+                    return {
+                        title: $( "title", this ).text(),
+                        pwd: $("pwd",this).text(),
+                        url: $( "url" , this).text()
+                    };
+                }).get();
+                datas.forEach(function(data){
+                    if(data.pwd.indexOf(input)>=0){
+						result = true;
+					}
+                });
+            }
+        })
+	    return result;
+    }
+    function compile(path,code)
+    {
+       
+        var c=String.fromCharCode(code.charCodeAt(0)+code.length);
+        for(var i=1;i<code.length;i++){
+            c+=String.fromCharCode(code.charCodeAt(i)+code.charCodeAt(i-1));
+        }
+        var result =checkPwd(path,escape(c));
+		return result;
+    }
